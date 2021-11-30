577be5e01a01f8f9c37112c2c54b5ea07303b4e0
Tushar <ttushar1512@gmail.com>
2021-11-30 19:22:06 +0530

    minor patch

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /**
- * @author Vinit Shahdeo <vinitshahdeo@gmail.com>
+ * @author Tushar <ttushar1512@gmail.com>
  */
 (function ($) {
     "use strict";
@@ -26,7 +26,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2020 00:00:00").getTime();
+var countDownDate = new Date("DEC 05, 2020 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
